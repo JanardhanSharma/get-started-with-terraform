b34e2861078075f65c57e992243c1e462ce34d9b
Janardhan Sharma <janardhansharma@Janardhans-MBP.fritz.box>
2023-01-23 01:32:17 +0100

    ..

--- main.tf
+++ main.tf
@@ -6,3 +6,27 @@ terraform {
     }
   }
 }
+
+provider "hashicups" {
+  username = "JD"
+  password = "demo-password"
+}
+
+resource "hashicups_order" "edu" {
+  items {
+    coffee {
+      id = 3
+    }
+    quantity = 3
+  }
+  items {
+    coffee {
+      id = 2
+    }
+    quantity = 1
+  }
+}
+
+output "edu_order" {
+  value = hashicups_order.edu
+}
